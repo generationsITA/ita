f840b141e6552a681a06627f89e832a1e25c9661
Dima <dim.kry.lis@gmail.com>
2020-05-06 23:22:00 +0300

    store removed from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,31 +5,11 @@ import "./styles/index.css";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import rootReducer from "./store/root.reducer";
 import { ThemeProvider, StylesProvider } from "@material-ui/core/styles";
 import { theme } from "./index.style";
-import { createStore, compose, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
 import "./styles/index.css";
-import createSagaMiddleware from "redux-saga";
-import rootSaga from "./store/root.saga";
 import { GlobalCSS } from "./index.style";
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware, thunkMiddleware];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
-  compose ||
-  applyMiddleware(...middleware);
-
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
-
-sagaMiddleware.run(rootSaga);
+import { store } from './store/store';
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/store.ts
@@ -0,0 +1,23 @@
+import rootReducer from "./root.reducer";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./root.saga";
+
+declare global {
+    interface Window {
+      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+  }
+  
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [sagaMiddleware, thunkMiddleware];
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+    compose ||
+    applyMiddleware(...middleware);
+  
+  export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
+  
+  sagaMiddleware.run(rootSaga);
+  const { dispatch } = store;
+  export type Dispatch = typeof dispatch;
