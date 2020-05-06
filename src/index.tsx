915f556fa89b1a82d44d60b8172ae609aaaba735
Dima <dim.kry.lis@gmail.com>
2020-05-06 17:26:24 +0300

    redux extension added

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,27 @@ import { Provider } from "react-redux";
 import rootReducer from "./store/root.reducer";
 import { ThemeProvider, StylesProvider } from "@material-ui/core/styles";
 import { theme } from "./index.style";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import "./styles/index.css";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./store/root.saga";
 import { GlobalCSS } from "./index.style";
 
-
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware, thunkMiddleware];
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  compose ||
+  applyMiddleware(...middleware);
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
+
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
