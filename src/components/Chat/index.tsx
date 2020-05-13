ab0569232fbb475f15dd020fc56506a90b95083e
Dmytro Krysanov <dim.kry.lis@gmail.com>
2020-05-13 15:37:54 +0300

    router added

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './Chat.css';
+// import queryString from 'query-string';
 import ChatHeader from './ChatHeader/ChatHeader';
 import MessageList from './MessageList/MessageList';
 import AddMessage from './AddMessage/AddMessage';
+import io from 'socket.io-client';
 
 const list = [
   'message 1', 'message 2', 'message 3', 'message 4', 'message 5', 'message 6', 'message 4', 'message 5', 'message 6'
